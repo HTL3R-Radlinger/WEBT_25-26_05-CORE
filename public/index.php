48969bfaafc7d97dd3e9a7cf477c5b06f0c55643
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-24 16:56:27 +0100

    US1

--- public/index.php
+++ public/index.php
@@ -1,18 +1,44 @@
 <?php
-$preferences = [];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['type_appetizer'])) {
-        $preferences[] = 'type_appetizer';
-    }
-    if (isset($_POST['type_mainCourse'])) {
-        $preferences[] = 'type_mainCourse';
-    }
-    if (isset($_POST['type_dessert'])) {
-        $preferences[] = 'type_dessert';
-    }
-    setcookie("preferences", json_encode($preferences), time() + 600, "/", "localhost");
+use JetBrains\PhpStorm\NoReturn;
+
+$storedPreferences = [];
+
+if (isset($_COOKIE['preferences'])) {
+    $storedPreferences = json_decode($_COOKIE['preferences'], true);
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['deleteCookie'])) {
+        deleteCookie();
+    }
+    if (isset($_POST['updateCookie'])) {
+        $preferences = [];
+        if (isset($_POST['type_appetizer'])) {
+            $preferences[] = 'type_appetizer';
+        }
+        if (isset($_POST['type_mainCourse'])) {
+            $preferences[] = 'type_mainCourse';
+        }
+        if (isset($_POST['type_dessert'])) {
+            $preferences[] = 'type_dessert';
+        }
+
+        setcookie("preferences", json_encode($preferences), time() + 600, "/", "localhost");
+
+        // Redirect -> new Request -> Cookie is visible
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    }
+}
+#[NoReturn]
+function deleteCookie(): void
+{
+    setcookie('preferences', "", time() - 1200);
+    unset($_COOKIE['preferences']);
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -27,28 +53,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 <h2>Meal Types</h2>
 <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    <label for="type_appetizer">
-        <input type="checkbox" name="type_appetizer" id="type_appetizer">
+    <label>
+        <input type="checkbox"
+               name="type_appetizer"
+                <?= in_array('type_appetizer', $storedPreferences) ? 'checked' : '' ?>
+        >
         Appetizer
     </label>
     <hr>
-    <label for="type_mainCourse">
-        <input type="checkbox" name="type_mainCourse" id="type_mainCourse">
+    <label>
+        <input type="checkbox"
+               name="type_mainCourse"
+                <?= in_array('type_mainCourse', $storedPreferences) ? 'checked' : '' ?>
+        >
         Main Course
     </label>
     <hr>
-    <label for="type_dessert">
-        <input type="checkbox" name="type_dessert" id="type_dessert">
+    <label>
+        <input type="checkbox"
+               name="type_dessert"
+                <?= in_array('type_dessert', $storedPreferences) ? 'checked' : '' ?>
+        >
         Dessert
     </label>
     <hr>
-    <input type="submit" name="submit" value="Send">
+    <input type="submit" name="updateCookie" value="Save to Cookie">
 </form>
-<?php
-echo $_COOKIE["preferences"];
-if (!empty($preferences)) {
-    echo '<pre>' . json_encode($preferences, JSON_PRETTY_PRINT) . '</pre>';
-}
-?>
+<form method="POST" action="<?= $_SERVER['PHP_SELF']; ?>">
+    <input type="submit" name="deleteCookie" value="Reset Preferences">
+</form>
 </body>
 </html>
